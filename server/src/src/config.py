b2e1a462c841b7c618f611c81bdd82ca99b8d514
AbhayAravinda <33093450+AbhayAravinda@users.noreply.github.com>
2020-10-07 22:22:34 +0530

    Add files via upload

---
+++ server/src/src/config.py
@@ -0,0 +1,8 @@
+def get_configurations():
+	return{
+		"client_id":"removed while uploading to github",
+		"client_secret":"removed while uploading to github",
+		"user_agent":"removed while uploading to github",
+		"redirect_uri":"http://localhost:8000"
+	}
+	
