d84e1a9e4057a41d8a79d2406dbd863878a7934f
AbhayAravinda <33093450+AbhayAravinda@users.noreply.github.com>
2020-10-07 22:23:13 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-def get_configurations():
-	return{
-		"client_id":"removed while uploading to github",
-		"client_secret":"removed while uploading to github",
-		"user_agent":"removed while uploading to github",
-		"redirect_uri":"http://localhost:8000"
-	}
-	
